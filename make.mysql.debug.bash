fff4e234f3baa3fab44fbf3036afb05c7d6113bc
Rik Prohaska <prohaska7@gmail.com>
2015-08-06 10:28:22 -0400

    build tokudb + mysql 5.7.8

--- make.mysql.debug.bash
+++ make.mysql.debug.bash
@@ -1,15 +1,17 @@
 # make a debug build of mysql 5.7 with tokudb
 # no patch to mysql 5.7 is required
 set -e
-#git clone -b 5.7 git@github.com:mysql/mysql-server
-tar xzf ~/mysql-5.7.8-rc.tar.gz
-git clone -b my577.json git@github.com:prohaska7/tokudb-engine
+git clone -b 5.7 git@github.com:mysql/mysql-server
+pushd mysql-server
+git checkout mysql-5.7.8
+popd
+git clone -b my578p git@github.com:prohaska7/tokudb-engine
 git clone -b master git@github.com:prohaska7/ft-index
-ln -s ../../tokudb-engine/storage/tokudb mysql-5.7.8-rc/storage/tokudb
+ln -s ../../tokudb-engine/storage/tokudb mysql-server/storage/tokudb
 ln -s ../../../ft-index tokudb-engine/storage/tokudb/ft-index
 mkdir build
 pushd build install
 # may need to tinker with the cmake options
-GCC=gcc-4.8 CXX=g++-4.8 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../install -DUSE_VALGRIND=ON -DTOKUDB_NOPATCH_CONFIG=1 -DDOWNLOAD_BOOST=1 -DWITH_BOOST=$HOME/boost -DZLIB_INCLUDE_DIR=/usr/include ../mysql-5.7.8-rc
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../install -DTOKUDB_NOPATCH_CONFIG=1 -DDOWNLOAD_BOOST=1 -DWITH_BOOST=$HOME/boost -DZLIB_INCLUDE_DIR=/usr/include ../mysql-server
 make -j8 install
 popd
