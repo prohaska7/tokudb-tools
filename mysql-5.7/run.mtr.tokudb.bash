42affbfd0c47495f40408e26833196429c61287b
Rik Prohaska <prohaska7@gmail.com>
2016-07-21 20:56:52 -0400

    simplify tokudb tests on percona server

--- mysql-5.7/run.mtr.tokudb.bash
+++ mysql-5.7/run.mtr.tokudb.bash
@@ -1,9 +1,11 @@
 for suite in tokudb.add_index tokudb.alter_table tokudb tokudb.bugs tokudb.parts tokudb.rpl tokudb.sys_vars ; do
-    ./mtr --suite=$suite --skip-test=fast_up.* --mysqld='--plugin-load=tokudb=ha_tokudb.so;tokudb_trx=ha_tokudb.so;tokudb_locks=ha_tokudb.so;tokudb_lock_waits=ha_tokudb.so;tokudb_fractal_tree_info=ha_tokudb.so;tokudb_background_job_status=ha_tokudb.so' --mysqld=--loose-tokudb-check-jemalloc=0 --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --big-test >$suite.out 2>&1
+    rm -rf var $suite.var
+    ./mtr --suite=$suite --skip-test=fast_up.* --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --big-test >$suite.out 2>&1
     mv var $suite.var
 done
 
 for suite in funcs iuds ; do
+    rm -rf var engines.$suite.var
     ./mtr --suite=engines/$suite --mysqld=--default-storage-engine=tokudb --mysqld=--default-tmp-storage-engine=tokudb --mysqld=--plugin-load=tokudb=ha_tokudb.so --mysqld=--loose-tokudb-check-jemalloc=0 --parallel=auto --force --retry=0 --max-test-fail=0 --big-test >engines.$suite.out 2>&1
     mv var engines.$suite.var
 done
