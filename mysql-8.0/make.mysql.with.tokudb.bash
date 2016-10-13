25731e52a59f9050b8bfb3d9b99788d2f106b09a
Rik Prohaska <prohaska7@gmail.com>
2016-10-13 13:14:43 -0400

    build a debug mysql-8.0 with tokudb

---
+++ mysql-8.0/make.mysql.with.tokudb.bash
@@ -0,0 +1,20 @@
+set -e
+
+git clone -b 8.0-tokudb git@github.com:prohaska7/mysql-server
+git clone -b my800 git@github.com:prohaska7/percona-server
+git clone -b my800 git@github.com:prohaska7/tokuft
+
+ln -s ../../percona-server/storage/tokudb mysql-server/storage/tokudb
+
+rmdir percona-server/storage/tokudb/PerconaFT
+ln -s ../../../tokuft percona-server/storage/tokudb/PerconaFT
+
+for x in $(ls -d percona-server/mysql-test/suite/tokudb*); do
+    ln -s ../../../$x mysql-server/mysql-test/suite/$(basename $x)
+done
+
+mkdir build install
+pushd build
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../install -DDOWNLOAD_BOOST=1 -DWITH_BOOST=$HOME/boost -DZLIB_INCLUDE_DIR=/usr/include -DINSTALL_SQLBENCHDIR=../install ../mysql-server >cmake.out 2>&1
+make -j8 install >make.out 2>&1
+popd
