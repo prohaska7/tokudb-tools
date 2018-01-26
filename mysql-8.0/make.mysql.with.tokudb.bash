6e3ad824cae38e14d50fe526cc5f6cb772d84076
rik prohaska <prohaska7@gmail.com>
2018-01-25 20:34:55 -0500

    build tokudb in mysql 8.0.4

--- mysql-8.0/make.mysql.with.tokudb.bash
+++ mysql-8.0/make.mysql.with.tokudb.bash
@@ -1,12 +1,12 @@
 set -e
 
-git clone -b tokudb-803 git@github.com:prohaska7/mysql-server
-git clone -b tokudb-803 git@github.com:prohaska7/percona-server
+git clone -b tokudb-804 git@github.com:prohaska7/mysql-server
+git clone -b tokudb-804 git@github.com:prohaska7/percona-server
 git clone -b tokudb-803 git@github.com:prohaska7/tokuft
 
 ln -s ../../percona-server/storage/tokudb mysql-server/storage/tokudb
 
-rmdir percona-server/storage/tokudb/PerconaFT
+rm percona-server/storage/tokudb/PerconaFT
 ln -s ../../../tokuft percona-server/storage/tokudb/PerconaFT
 
 ln -s ../../../percona-server/mysql-test/include/have_tokudb.inc mysql-server/mysql-test/include/have_tokudb.inc
@@ -16,6 +16,6 @@ done
 
 mkdir build install
 pushd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../install -DMYSQL_MAINTAINER_MODE=ON -DDOWNLOAD_BOOST=1 -DWITH_BOOST=$HOME/projects/boost -DZLIB_INCLUDE_DIR=/usr/include -DINSTALL_SQLBENCHDIR=../install -DTOKUDB_NOPATCH_CONFIG=1 ../mysql-server >cmake.out 2>&1
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../install -DMYSQL_MAINTAINER_MODE=ON -DDOWNLOAD_BOOST=1 -DWITH_BOOST=$HOME/boost -DZLIB_INCLUDE_DIR=/usr/include -DINSTALL_SQLBENCHDIR=../install -DTOKUDB_NOPATCH_CONFIG=1 ../mysql-server >cmake.out 2>&1
 make -j8 install >make.out 2>&1
 popd
