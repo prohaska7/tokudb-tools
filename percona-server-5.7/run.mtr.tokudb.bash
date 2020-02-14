3fc34bc1fb694d02d3b04e7c27131bdad3737ba8
rik prohaska <prohaska7@gmail.com>
2020-02-14 10:39:31 -0500

    Compile with gcc-9 and clang-9. Run mysql-tests on debug build. Run msyql-tests on debug build with valgrind. Found memory leak in tokudb.parts upgrade_from_prev_ver test. Run mysql-tests on ASAN build with clang.  Add tokuft/portability/memory.cc patch. Found memory leak in tokudb.parts upgrade_from_prev_ver test.

--- percona-server-5.7/run.mtr.tokudb.bash
+++ percona-server-5.7/run.mtr.tokudb.bash
@@ -1,17 +1,33 @@
+mtr_options=
+ld_preload=$LD_PRELOAD
+
+for arg in $* ; do
+    if [[ $arg =~ --mtr-option=(.*) ]] ; then
+        mtr_options="$mtr_options ${BASH_REMATCH[1]}"
+    fi
+done
+
 engine=tokudb
 
 for suite in $(basename -a suite/tokudb*); do
     if [ ! -f $suite.out ] ; then
         rm -rf var $suite.var
-        ./mtr --suite=$suite --skip-test=fast_up.* --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --big-test >$suite.out 2>&1
+        if [[ $suite =~ backup ]] ; then
+            old_ld_preload=$ld_preload
+            ld_preload=$ld_preload:$PWD/../lib/libHotBackup.so
+        fi
+        LD_PRELOAD=$ld_preload ./mtr --suite=$suite --skip-test=fast_up.* --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --big-test $mtr_options >$suite.out 2>&1
         mv var $suite.var
+        if [[ $suite =~ backup ]] ; then
+            ld_preload=$old_ld_preload
+        fi
     fi
 done
 
 for suite in funcs iuds ; do
     if [ ! -f $engine.engines.$suite.out ] ; then
         rm -rf var $engine.engines.$suite.var
-        ./mtr --suite=engines/$suite --mysqld=--default-storage-engine=$engine --mysqld=--default-tmp-storage-engine=$engine --mysqld=--plugin-load=tokudb=ha_tokudb.so --mysqld=--loose-tokudb-check-jemalloc=0 --parallel=auto --force --retry=0 --max-test-fail=0 --big-test >$engine.engines.$suite.out 2>&1
+        ./mtr --suite=engines/$suite --mysqld=--default-storage-engine=$engine --mysqld=--default-tmp-storage-engine=$engine --mysqld=--plugin-load=tokudb=ha_tokudb.so --mysqld=--loose-tokudb-check-jemalloc=0 --parallel=auto --force --retry=0 --max-test-fail=0 --big-test $mtr_options >$engine.engines.$suite.out 2>&1
         mv var $engine.engines.$suite.var
     fi
 done
@@ -21,7 +37,7 @@ engine=innodb
 for suite in $(basename -a suite/innodb*); do
     if [ ! -f $suite.out ] ; then
         rm -rf var $engine.var
-        ./mtr --suite=$suite --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --big-test >$suite.out 2>&1
+        ./mtr --suite=$suite --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --big-test $mtr_options >$suite.out 2>&1
         mv var $suite.var
     fi
 done
@@ -29,7 +45,7 @@ done
 for suite in funcs iuds ; do
     if [ ! -f $engine.engines.$suite.out ] ; then
         rm -rf var $engine.engines.$suite.var
-        ./mtr --suite=engines/$suite --mysqld=--default-storage-engine=$engine --mysqld=--default-tmp-storage-engine=$engine --parallel=auto --force --retry=0 --max-test-fail=0 --big-test >$engine.engines.$suite.out 2>&1
+        ./mtr --suite=engines/$suite --mysqld=--default-storage-engine=$engine --mysqld=--default-tmp-storage-engine=$engine --parallel=auto --force --retry=0 --max-test-fail=0 --big-test $mtr_options >$engine.engines.$suite.out 2>&1
         mv var $engine.engines.$suite.var
     fi
 done
--- percona-server-5.7/run.mtr.tokudb.valgrind.bash
+++ percona-server-5.7/run.mtr.tokudb.valgrind.bash
@@ -1,9 +1,20 @@
-for suite in tokudb.add_index tokudb.alter_table tokudb tokudb.bugs tokudb.parts tokudb.rpl tokudb.sys_vars ; do
-    ./mtr --suite=$suite --valgrind-mysqld --skip-test=fast_up.*\|rows-32m.*\|hotindex-del-.*\|change_column_char\|change_column_bin\|change_column_all_1000.* --mysqld='--plugin-load=tokudb=ha_tokudb.so;tokudb_trx=ha_tokudb.so;tokudb_locks=ha_tokudb.so;tokudb_lock_waits=ha_tokudb.so;tokudb_fractal_tree_info=ha_tokudb.so;tokudb_background_job_status=ha_tokudb.so' --mysqld=--loose-tokudb-check-jemalloc=0 --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60  >valgrind.$suite.out 2>&1
-    mv var valgrind.$suite.var
+# valgrind_options="--valgrind-mysqld"
+valgrind_options="--valgrind-mysqld --valgrind-option=-s"
+# valgrind_options="--valgrind-mysqld --valgrind-option=--leak-check=full --valgrind-option=--show-reachable=yes"
+# valgrind_options="--valgrind-mysqld --valgrind-option=--track-origins=yes --valgrind-option=-s"
+
+tokudb_plugins="tokudb=ha_tokudb.so;tokudb_trx=ha_tokudb.so;tokudb_locks=ha_tokudb.so;tokudb_lock_waits=ha_tokudb.so;tokudb_fractal_tree_info=ha_tokudb.so;tokudb_background_job_status=ha_tokudb.so;tokudb_file_map=ha_tokudb.so"
+
+for suite in $(basename -a suite/tokudb*|egrep -v backup) ; do
+    if [ ! -f valgrind.$suite.out ] ; then
+        ./mtr --suite=$suite $valgrind_options --skip-test=fast_up.*\|rows-32m.*\|hotindex-del-.*\|change_column_char\|change_column_bin\|change_column_all_1000.* --mysqld=--plugin-load=$tokudb_plugins --mysqld=--loose-tokudb-check-jemalloc=0 --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60  >valgrind.$suite.out 2>&1
+            mv var valgrind.$suite.var
+    fi
 done
 
 for suite in funcs iuds ; do
-    ./mtr --suite=engines/$suite --valgrind-mysqld --mysqld=--default-storage-engine=tokudb --mysqld=--default-tmp-storage-engine=tokudb --mysqld=--plugin-load=tokudb=ha_tokudb.so --mysqld=--loose-tokudb-check-jemalloc=0 --parallel=auto --force --retry=0 --max-test-fail=0 >valgrind.engines.$suite.out 2>&1
-    mv var valgrind.engines.$suite.var
+    if [ ! -f valgrind.engines.$suite.out ] ; then
+        ./mtr --suite=engines/$suite $valgrind_options --mysqld=--default-storage-engine=tokudb --mysqld=--default-tmp-storage-engine=tokudb --mysqld=--plugin-load=tokudb=ha_tokudb.so --mysqld=--loose-tokudb-check-jemalloc=0 --parallel=auto --force --retry=0 --max-test-fail=0 >valgrind.engines.$suite.out 2>&1
+        mv var valgrind.engines.$suite.var
+    fi
 done
